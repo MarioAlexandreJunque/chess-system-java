c269ddbd44da18fa488c5c5fc485dfd381c8b86d
Mario Alexandre Junque <marjunque@hotmail.com>
2021-02-16 20:16:52 -0300

    ChessException and ChessPosition

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -6,8 +6,8 @@ public class ChessPosition {
 	private char column;
 	private int row;
 
-	public ChessPosition(char column, int row2) {
-		if (column < 'a' || column > 'h' || row2 > 1 || row2 > 8) {
+	public ChessPosition(char column, int row) {
+		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
 			throw new ChessException("Error instantiating ChessPosition. Valid values ar from a1 to h8.");
 		}
 		this.column = column;
